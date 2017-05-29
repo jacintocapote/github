177bab6a5ea9b655e8af6d1ee39ab08083c81f87
Jacinto capote Robles <jacintocapote@gmail.com>
2017-05-29 22:29:35 +0200

    Added settings page for insert API token instead hard code

---
+++ src/Form/githubSettingsForm.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\github\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * A basic form to save github token.
+ */
+class githubSettingsForm extends ConfigFormBase {
+  /** 
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'github_token_settings';
+  }
+
+  /** 
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'github.settings',
+    ];
+  }
+
+  /** 
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('github.settings');
+
+    $form['github_token'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Github API Token'),
+      '#default_value' => $config->get('token'),
+      '#description' => $this->t('Please access to https://github.com/settings/tokens to get more information'),
+    );  
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /** 
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('github.settings')
+      ->set('token', $form_state->getValue('github_token'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
--- src/GithubGetClient.php
+++ src/GithubGetClient.php
@@ -14,13 +14,13 @@ class GithubGetClient {
    * On construct asign github api token.
    */
   public function __construct() {
-    //Pending read from variable and assing value.
-    $api_token = '4b432961af4f7ca2566e00670f5fb5239c3c39d8';
     github_load_library();
+    $config = $this->config('github.settings');
+    $api_token = $config->get('token');
+
     $client = new \GithubClient();
     $client->setAuthType(\GitHubClientBase::GITHUB_AUTH_TYPE_OAUTH_BASIC);
     $client->setOauthKey($api_token);
-
     $this->client = $client;
   }
 
